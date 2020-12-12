0fc34ba416ac6b6d6f58db137969854ffca274a5
sharo <sharonaizn@gmail.com>
2020-12-12 23:23:02 +0200

    adding BFS

--- quoridor-logic-server/src/main/java/com/rl/mpquoridor/models/Board.java
+++ quoridor-logic-server/src/main/java/com/rl/mpquoridor/models/Board.java
@@ -3,6 +3,8 @@ package com.rl.mpquoridor.models;
 import com.rl.mpquoridor.enums.Direction;
 import lombok.Data;
 
+import java.util.LinkedList;
+
 @Data
 public class Board {
     private int[][] board;
@@ -14,24 +16,11 @@ public class Board {
     }
 
     public int[][] addWall(Wall wall) {
-        Direction direction = wall.getDirection();
-        Position position = wall.getPosition();
-        int newX = position.getX() * 2 + 1;
-        int newY = position.getY() * 2 + 1;
-        if (direction == Direction.Right) {
-            this.board[newY][newX] = 1;
-            this.board[newY][newX + 1] = 1;
-            this.board[newY][newX + 2] = 1;
-        } else {
-            this.board[newY][newX] = 1;
-            this.board[newY + 1][newX] = 1;
-            this.board[newY + 2][newX] = 1;
-        }
-
+        this.board = this.addWallToBoard(wall, this.board);
         return this.board;
     }
 
-    public boolean checkAddingWall(Wall wall) {
+    public boolean checkAddingWall(Wall wall, Position[] positions, int[] heightsWin) {
         Direction direction = wall.getDirection();
         Position position = wall.getPosition();
         int newX = position.getX() * 2 + 1;
@@ -59,10 +48,7 @@ public class Board {
             }
         }
 
-        //Todo: check if there still option to play
-
-
-        return true;
+        return this.checkPaths(wall, positions, heightsWin);
     }
 
     private void initBoard() {
@@ -71,5 +57,106 @@ public class Board {
                 this.board[height][width] = 0;
             }
         }
+    }
+
+    private boolean checkPaths(Wall wall, Position[] positions, int[] heightsWin) {
+        int[][] newBoard = new int[this.board.length][this.board[0].length];
+        for (int height = 0; height < this.board.length; height++) {
+            System.arraycopy(this.board[height], 0, newBoard[height], 0, this.board[0].length);
+        }
+
+        newBoard = this.addWallToBoard(wall, newBoard);
+
+        for (int i = 0; i < positions.length; i++) {
+            if (this.checkingExistingPath(newBoard, positions[i], heightsWin[i])) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    //BFS
+    // 1 - checked
+    // 0 - not checked
+    private boolean checkingExistingPath(int[][] board, Position position, int rawExit) {
+        int explored = 1;
+
+        LinkedList<Position> nextToVisit
+                = new LinkedList<>();
+        nextToVisit.add(position);
+
+        while (!nextToVisit.isEmpty()) {
+            Position cur = nextToVisit.remove();
+
+            if (!this.isValidLocation(cur)
+                    || board[cur.getY()][cur.getX()] == explored) {
+                continue;
+            }
+
+            // Exit - need to change for four players
+            if (cur.getY() == rawExit) {
+                return true;
+            }
+
+            //RIGHT
+            if (board[cur.getY()][cur.getX() + 1] != 1) {
+                Position Position
+                        = new Position(
+                        cur.getX() + 2,
+                        cur.getY());
+                nextToVisit.add(Position);
+            }
+            // LEFT
+            else if (board[cur.getY()][cur.getX() - 1] != 1) {
+                Position Position
+                        = new Position(
+                        cur.getX() - 2,
+                        cur.getY());
+                nextToVisit.add(Position);
+            }
+            // UP
+            else if (board[cur.getY() + 1][cur.getX()] != 1) {
+                Position Position
+                        = new Position(
+                        cur.getX(),
+                        cur.getY() + 2);
+                nextToVisit.add(Position);
+            }
+            // DOWN
+            else if (board[cur.getY() - 1][cur.getX()] != 1) {
+                Position Position
+                        = new Position(
+                        cur.getX(),
+                        cur.getY() - 2);
+                nextToVisit.add(Position);
+            }
+            board[cur.getY()][cur.getX()] = 2;
+        }
+        return true;
+    }
+
+    public int[][] addWallToBoard(Wall wall, int[][] board) {
+        Direction direction = wall.getDirection();
+        Position position = wall.getPosition();
+        int newX = position.getX() * 2 + 1;
+        int newY = position.getY() * 2 + 1;
+        if (direction == Direction.Right) {
+            board[newY][newX] = 1;
+            board[newY][newX + 1] = 1;
+            board[newY][newX + 2] = 1;
+        } else {
+            board[newY][newX] = 1;
+            board[newY + 1][newX] = 1;
+            board[newY + 2][newX] = 1;
+        }
+
+        return board;
+    }
+
+    private boolean isValidLocation(Position position) {
+        int size = this.board.length;
+        int x = position.getX();
+        int y = position.getY();
+        return x >= 0 && x < size && y >= 0 && y < size;
     }
 }

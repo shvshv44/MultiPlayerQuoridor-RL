a502dbec53fe46db11085c3766e07a77565a740d
Shaked <shvshv44@gmail.com>
2020-12-22 20:59:21 +0200

    add api templates

--- quoridor-logic-server/src/main/java/com/rl/mpquoridor/controllers/GameAPIController.java
+++ quoridor-logic-server/src/main/java/com/rl/mpquoridor/controllers/GameAPIController.java
@@ -1,17 +1,36 @@
 package com.rl.mpquoridor.controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class GameAPIController {
 
 
-    @GetMapping("/startGame")
+    @GetMapping("/CreateGame")
     @ResponseBody
-    public String startGame() {
+    public String createGame() {
         return "Game has been started!";
+    }
+
+    @GetMapping("/JoinGame/{gameId}")
+    @ResponseBody
+    public String joinGame(@PathVariable String gameId) {
+        return "Join to game id " + gameId;
+    }
+
+    @GetMapping("/History")
+    @ResponseBody
+    public List<String> history() {
+        return new ArrayList<>();
+    }
+
+    @GetMapping("/History/{gameId}")
+    @ResponseBody
+    public String historyByGameId(@PathVariable String gameId) {
+        return "Get all history of: " + gameId;
     }
 
 }

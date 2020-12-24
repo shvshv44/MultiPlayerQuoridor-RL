5162f825e52a7ae1dab0421d231686d261492d1d
guyra <guyraviv1997@gmail.com>
2020-12-24 11:28:29 +0200

    Merge branches 'master' and 'web-socket-for-java' of https://github.com/shvshv44/MultiPlayerQuoridor-RL into web-socket-for-java
    # Conflicts:
    #	quoridor-logic-server/src/main/java/com/rl/mpquoridor/models/actions/TurnAction.java

--- quoridor-logic-server/src/main/java/com/rl/mpquoridor/controllers/GameWebSocket.java
+++ quoridor-logic-server/src/main/java/com/rl/mpquoridor/controllers/GameWebSocket.java
@@ -1,8 +1,8 @@
 package com.rl.mpquoridor.controllers;
 
-import com.rl.mpquoridor.models.actions.MovePawn;
-import com.rl.mpquoridor.models.actions.PlaceWall;
 import com.rl.mpquoridor.models.actions.TurnAction;
+import com.rl.mpquoridor.models.events.NewTurnEvent;
+import com.rl.mpquoridor.models.events.TurnActionEvent;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
@@ -17,19 +17,19 @@ public class GameWebSocket {
 
 
     @MessageMapping("/{gameId}/movePawn")
-    public void movePawn(@PathVariable String gameId, MovePawn action) {
+    public void movePawn(@PathVariable String gameId, TurnActionEvent action) {
         // todo: play the movePawn and return the game status
         this.messageSender.convertAndSend("/topic/gameStatus/" + gameId, action);
     }
 
     @MessageMapping("/{gameId}/putWall")
-    public void putWall(@PathVariable String gameId, PlaceWall action) {
+    public void putWall(@PathVariable String gameId, TurnActionEvent action) {
         // todo: play the putWall and return the
         this.messageSender.convertAndSend("/topic/gameStatus/" + gameId, action);
     }
 
 
-    public void endTurn(String gameId, TurnAction action) {
+    public void endTurn(String gameId, NewTurnEvent action) {
         this.messageSender.convertAndSend("/topic/gameStatus/" + gameId, action);
     }
 }

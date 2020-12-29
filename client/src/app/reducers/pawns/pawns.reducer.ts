8579527acf67f2d11ce160cb9bc1c019a4c3be42
sharo <sharonaizn@gmail.com>
2020-12-29 16:56:07 +0200

    start

--- client/src/app/reducers/pawns/pawns.reducer.ts
+++ client/src/app/reducers/pawns/pawns.reducer.ts
@@ -2,13 +2,15 @@ import {Action, createReducer, on, State} from '@ngrx/store';
 import {Position} from '../../interfaces/position';
 import {setPawns} from './pawns.actions';
 
-export const pawns: Position[] = [];
+export const pawns: Map<string, Position[]> = new Map<string, Position[]>();
 
 const pawnsReducerAction = createReducer(
   pawns,
-  on(setPawns, (state: Position[], {pawns}) => ([...pawns])),
+  on(setPawns, (state: Position[], {pawns}) => ()),
 );
 
 export function pawnsReducer(state: Position[], action: Action): any {
   return pawnsReducerAction(state, action);
 }
+
+export function initPawns()

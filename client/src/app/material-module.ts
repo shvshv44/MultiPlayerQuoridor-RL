897e19a8a1dc836432f0b02fcc51e1dc06227599
sharo <sharonaizn@gmail.com>
2021-01-05 20:21:51 +0200

    guye and sharone fixing wall game ended move positions

--- client/src/app/material-module.ts
+++ client/src/app/material-module.ts
@@ -2,12 +2,14 @@ import {MatButtonModule} from '@angular/material/button';
 import {NgModule} from '@angular/core';
 import {MatListModule} from '@angular/material/list';
 import {MatCardModule} from '@angular/material/card';
+import {ClipboardModule} from '@angular/cdk/clipboard';
 
 @NgModule({
   exports: [
     MatButtonModule,
     MatListModule,
-    MatCardModule
+    MatCardModule,
+    ClipboardModule
   ]
 })
 export class MaterialModule {

d303187652582f80aa6f06bc29f6548ea8eb1e68
guyra <guyraviv1997@gmail.com>
2021-01-08 18:49:54 +0200

    join_game_random_player

---
+++ python-logic/main.py
@@ -0,0 +1,32 @@
+import requests
+# import asyncio
+import logging
+# import websockets
+# import stomper
+
+logging.basicConfig(level=logging.INFO)
+
+serverUrl = "http://localhost:8080"
+#
+# async def connect_to_game(game_id, player_name):
+#     websocket_url = 'ws://' + serverUrl + "/" + '/quoridor-websocket';
+#     websockets.connect()
+
+
+def join_game_random_player(game_id):
+    join_game_url = serverUrl + "/JoinGame/" + game_id + "/randomPlayer"
+    response = requests.get(join_game_url)
+
+    return response
+
+
+# def initial_websocket(game_id, player_name):
+#     join_game_url = serverUrl + "/JoinGame/" + game_id + "/randomPlayer"
+#     response = requests.get(join_game_url)
+#
+#     return response
+
+
+if __name__ == '__main__':
+    game_id = "e166a63d-9615-439b-9d18-5d8b4559ef94"
+    join_game_random_player(game_id)

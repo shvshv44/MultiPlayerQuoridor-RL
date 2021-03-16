9f6cfe16695444b6b853b35ab328b1aafe355731
Shaked <shvshv44@gmail.com>
2021-03-16 19:18:51 +0200

    feat: update board and some fixes

--- python-logic/quoridor_env.py
+++ python-logic/quoridor_env.py
@@ -1,16 +1,24 @@
 import gym
 from gym import spaces
+from numpy import zeros
 
 
 class QuoridorEnv(gym.Env):
 
-    def __init__(self, players_names, players_locations, players_targets, player_name):
-        self.players_targets = players_targets
-        self.players_locations = players_locations
-        self.players_names = players_names
-        self.player_name = player_name
+    """
+    players: {
+        index: number,
+        start_location: number,
+        name: string,
+        targets: [number]
+    }
 
-        self.current_board = self.init_board(players_locations)
+    """
+
+    def __init__(self, players, main_player_index):
+        self.players = players
+        self.main_player_index = main_player_index
+        self.board = self.init_board()
 
         self.action_space = spaces.Tuple((
             # choosing either to move or put wall
@@ -40,9 +48,9 @@ class QuoridorEnv(gym.Env):
     def step(self, action):
         assert self.action_space.contains(action)
 
-        self.update_board(action)
+        self.update_board(self.main_player_index, action)
         reward, done = self.calculate_reward()
-        return self.current_board, reward, done, self.get_info()
+        return self.board, reward, done, self.get_info()
 
     def reset(self):
         # TODO: implement
@@ -52,36 +60,22 @@ class QuoridorEnv(gym.Env):
         # TODO: implement
         return {}
 
-    def init_board(self, players_names, locations):
+    def init_board(self):
         return (
-            locations[players_names[0]],
-            locations[players_names[1]],
-            [[0, 0, 0, 0, 0, 0, 0, 0],
-             [0, 0, 0, 0, 0, 0, 0, 0],
-             [0, 0, 0, 0, 0, 0, 0, 0],
-             [0, 0, 0, 0, 0, 0, 0, 0],
-             [0, 0, 0, 0, 0, 0, 0, 0],
-             [0, 0, 0, 0, 0, 0, 0, 0],
-             [0, 0, 0, 0, 0, 0, 0, 0],
-             [0, 0, 0, 0, 0, 0, 0, 0]],
-            [[0, 0, 0, 0, 0, 0, 0, 0],
-             [0, 0, 0, 0, 0, 0, 0, 0],
-             [0, 0, 0, 0, 0, 0, 0, 0],
-             [0, 0, 0, 0, 0, 0, 0, 0],
-             [0, 0, 0, 0, 0, 0, 0, 0],
-             [0, 0, 0, 0, 0, 0, 0, 0],
-             [0, 0, 0, 0, 0, 0, 0, 0],
-             [0, 0, 0, 0, 0, 0, 0, 0]]
+            self.players[0].start_location,
+            self.players[1].start_location,
+            zeros(shape=(8, 8)),
+            zeros(shape=(8, 8))
         )
 
     def calculate_reward(self):
         reward = 0
         done = False
 
-        for player in self.players_names:
-            if self.players_targets[player].contains(self.players_locations[player]):
+        for player in self.players:
+            if player.targets.contains(player.location):
                 done = True
-                if player == self.player_name:
+                if player.index == self.main_player_index:
                     reward = 1
                 else:
                     reward = -1
@@ -89,5 +83,26 @@ class QuoridorEnv(gym.Env):
 
         return reward, done
 
-    def update_board(self, action):
-        print("TODO!: update the board!")
+    def update_board(self, player, action):
+        if action[0] == 0:
+            player.location = self.get_new_cell_position(player.location, action[1])
+        if action[0] == 1:
+            assert 0 <= action[2] <= 64*2
+            if action[2] < 64:
+                self.board[len(self.players)][action[2] // 8][action[2] % 8] = True
+            else:
+                self.board[len(self.players) + 1][(action[2] - 64) // 8][(action[2] - 64) % 8] = True
+
+
+    def get_new_cell_position(self, cur_location, direction):
+        if direction == 0:
+            cur_location -= 9  # UP
+        elif direction == 1:
+            cur_location += 9  # DOWN
+        elif direction == 2:
+            cur_location -= 1  # LEFT
+        elif direction == 3:
+            cur_location += 1  # RIGHT
+
+        assert 0 <= cur_location <= 81
+        return cur_location

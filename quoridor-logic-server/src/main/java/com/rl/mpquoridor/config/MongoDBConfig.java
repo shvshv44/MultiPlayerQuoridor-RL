6288579058e1147192b84deb0d7d1c463cdba953
Guy Eisenbach <guyeise5@gmail.com>
2021-03-22 23:15:55 +0200

    Getting password as environment variable instead of hardcoded

--- quoridor-logic-server/src/main/java/com/rl/mpquoridor/config/MongoDBConfig.java
+++ quoridor-logic-server/src/main/java/com/rl/mpquoridor/config/MongoDBConfig.java
@@ -4,7 +4,7 @@ public class MongoDBConfig {
     public static final String HOSTNAME = "cluster0-shard-00-00.yp5ll.mongodb.net:27017,cluster0-shard-00-01.yp5ll.mongodb.net:27017,cluster0-shard-00-02.yp5ll.mongodb.net:27017";
     public static final String REPLICA_SET_NAME = "atlas-vzvdu7-shard-0";
     public static final String USERNAME = "quoridor-logic-server";
-    public static final String PASSWORD = "uaeAdadS1FYfwfNI";
+    public static final String PASSWORD = System.getenv("MONGODB_PASSWORD");
     public static final String AUTHENTICATION_DATABASE = "admin";
     public static final String DB = "games";
     public static final String COLLECTION="games";

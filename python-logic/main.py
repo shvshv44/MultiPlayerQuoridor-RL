25f107a617a6073284f20ee593830f5f40f5dab4
guyra <guyraviv1997@gmail.com>
2021-03-22 21:39:52 +0200

    refactor tcp class

--- python-logic/main.py
+++ python-logic/main.py
@@ -15,7 +15,15 @@ def join_game_random_player(game_id):
     return response
 
 
+def get_game_id():
+    join_game_url = serverUrl + "/JoinGame/" + game_id + "/randomPlayer"
+    response = requests.get(join_game_url)
+
+    return response
+
+
 if __name__ == '__main__':
-    # game_id = "e166a63d-9615-439b-9d18-5d8b4559ef94"
+    game_id = "e166a63d-9615-439b-9d18-5d8b4559ef94"
     # join_game_random_player(game_id)
-    tcp = TCP()
+
+    tcp = TCP(game_id)

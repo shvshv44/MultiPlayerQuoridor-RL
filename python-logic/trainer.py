d38a0d0c47a2618a3f90cd440c77c054e4e702a7
Shaked <shvshv44@gmail.com>
2021-03-23 22:15:28 +0200

    create game for agent

--- python-logic/trainer.py
+++ python-logic/trainer.py
@@ -1,16 +1,23 @@
-
+import requests as http
 
 
 class Trainer:
 
-
     def __init__(self, agent):
         self.server = "http://localhost:8080"
         self.agent = agent
+        self.name = "Trainer"
 
     def start_training_session(self):
         game_id = self.create_game()
+        self.start_game_with_agent(game_id)
 
 
     def create_game(self):
-        return "shaq" # TODO: use http to create the game
+        create_game_url = self.server + "/CreateGame/" + self.name
+        response = http.get(create_game_url)
+        return response
+
+    def start_game_with_agent(self, game_id):
+        pass # TODO: implement after TCP refactoring
+

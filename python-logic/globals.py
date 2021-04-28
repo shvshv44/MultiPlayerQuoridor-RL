e1a271f39d52001966af5c8aa5f51f86df3666bb
guyra <guyraviv1997@gmail.com>
2021-04-28 21:45:06 +0300

    Change to the linux server rout

--- python-logic/globals.py
+++ python-logic/globals.py
@@ -2,7 +2,7 @@ from api import MovementDirection
 
 class Global:
     server = "http://193.106.55.110:8080"
-    competitive_agent = "http://localhost:8001"
+    competitive_agent = "http://193.106.55.110:8001"
     movements = list(map(lambda c: c, MovementDirection))
 
     # 0-3: for UP, DOWN, LEFT RIGHT, 4 - 68: for 64 Horizontal walls, 69 - 131: for 64 Vertical walls
